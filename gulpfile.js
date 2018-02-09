068cc55e63cb8842f18c8d1cfd66ded8b2f3c867
Andrey Bulaev <bulayka89@gmail.com>
2018-02-09 18:39:33 +0300

    Исправлена ошибка при которой не копировались скрипты в папку билд. Подключен полифилл, исправлен блок production-process(в ИЕ съезжал вправо)

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js"
+    "source/js/**"
   ], {
     base: "source"
   })
